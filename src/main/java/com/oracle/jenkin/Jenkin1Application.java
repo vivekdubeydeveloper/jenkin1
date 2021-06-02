62039b82bb2628618e7367a300e58d0688603fde
VKDUBEY-IN\vkdubey <abc@test.com>
2021-06-02 16:03:47 +0530

    initial commit

---
+++ src/main/java/com/oracle/jenkin/Jenkin1Application.java
@@ -0,0 +1,13 @@
+package com.oracle.jenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkin1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jenkin1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/oracle/jenkin/Jenkin1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.jenkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkin1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
